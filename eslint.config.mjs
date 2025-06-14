b0a51282f38f1be91830174d07a47ef813147017
mhmitas <mahfuzulmitas@gmail.com>
2025-06-14 20:21:05 +0600

    Update ESLint configuration to disable additional rules for improved compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@next/next/no-html-link-for-pages": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
+
   },
 ];
 
